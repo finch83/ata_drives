b31956de91f98f51a844d3197a5ead0a0b5a8a98
Anton Melnyk <anton.melnyk@avid.com>
2019-12-24 16:49:35 +0200

    en/dis ebling cache works

--- ata_set.c
+++ ata_set.c
@@ -7,42 +7,37 @@
 
 int main(int argc, char *argv[])
 {
+/*
     unsigned char shift = 0;
     shift |= 1 << 7;
     printf("Shift: %d\n", shift);
-
+*/
     int fd;
 
     if ( openDrive(argv[1], &fd) != 0 )
     {
-        printf("Exited: openDrive return error\n");
         exit(1);
     }
 
-    unsigned char set_cache = 0;
+    unsigned char set_cache = SETFEATURES_DIS_WCACHE;
     if ( 3 == argc )
     {
         set_cache = (argv[2][0] - '0') == 0 ?
                     SETFEATURES_DIS_WCACHE :
                     SETFEATURES_EN_WCACHE;
-        printf("%sabling cahche\n", set_cache==SETFEATURES_DIS_WCACHE ? "Dis" : "En");
     }
-    else
-    {
-        printf("Disable cache by default\n");
-    }
+    printf("%sabling cahche\n", set_cache==SETFEATURES_DIS_WCACHE ? "Dis" : "En");
 
     if ( setFeature(fd, set_cache) != 0 )
     {
         printf("setFeature return error\n");
     }
-
+/*
     if ( getFeature3(fd, 0) != 0 )
     {
         printf("setFeature return error\n");
     }
-
-
+*/
 
 exit(0);
 }
